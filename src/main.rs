830a781ced77da525f062a6b1da282e86af21a86
willemml <willem@leit.so>
2020-11-01 18:18:22 -0800

    start on making code cleaner and more useable, still broken

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,111 @@
+use mcproto_rs::Deserialize;
+
 fn main() {
+    let fifty_millis = std::time::Duration::from_millis(50);
     let handshake_spec = mcproto_rs::v1_16_3::HandshakeSpec {
         version: mcproto_rs::types::VarInt::from(753),
         server_address: "127.0.0.1".to_owned(),
         server_port: 25565,
-        next_state: mcproto_rs::v1_16_3::HandshakeNextState::Status
+        next_state: mcproto_rs::v1_16_3::HandshakeNextState::Status,
     };
     let handshake_packet = mcproto_rs::v1_16_3::Packet753::Handshake(handshake_spec);
-    let connector = async { 
-        println!("Connecting...");
-        if let Ok(mut server) = connect().await {
-            println!("Sending handshake packets...");
-            if let Ok(_) = server.write_packet(handshake_packet).await {
-                println!("Sent handshake packet successfully.");
-            };
-            if let Ok(_) = server.write_packet(mcproto_rs::v1_16_3::Packet753::StatusRequest(mcproto_rs::v1_16_3::StatusRequestSpec {})).await {
-                println!("Sent request packet successfully.");
-            };
-            println!("Receiving response...");
-            if let Ok(response) = server.read_packet::<mcproto_rs::v1_16_3::RawPacket753>().await {
-                use mcproto_rs::Deserialize;
-                match response {
-                    Some(packet) => {
-                        match packet {
-                            mcproto_rs::v1_16_3::RawPacket753::StatusResponse(body) => {
-                                if let Ok(status) = mcproto_rs::status::StatusSpec::mc_deserialize(body.data) {
-                                    println!("{:?}", status.value);
-                                } else {
-                                    println!("Bad packet data.");
-                                };
-                            },
-                            _other => println!("Wrong packet.")
-                        };
-                    },
-                    None => println!("No packet."),
-                };
-            } else {
-                println!("Failed to read response.");
-            };
-        } else {
-            println!("Failed to connect to server.")
-        };
-    };
-    tokio::runtime::Runtime::new().unwrap().block_on(connector);
-}
-
-async fn connect() -> tokio::io::Result<mctokio::TcpConnection> {
-    let connection = mctokio::TcpConnection::connect_to_server(std::net::SocketAddrV4::new(
-        std::net::Ipv4Addr::new(127, 0, 0, 1),
+    let mut server_connection = ServerConnection::new(std::net::SocketAddr::new(
+        std::net::IpAddr::V4(std::net::Ipv4Addr::new(127, 0, 0, 1)),
         25565,
     ));
-    Ok(connection.await?)
+    if server_connection.send_packet(handshake_packet) {
+        println!("Sent handshake packet.");
+    } else {
+        println!("Failed to send handshake packet.");
+    }
+    std::thread::sleep(std::time::Duration::from_millis(50));
+    server_connection.send_packet(mcproto_rs::v1_16_3::Packet753::StatusRequest(
+        mcproto_rs::v1_16_3::StatusRequestSpec {},
+    ));
+    std::thread::sleep(fifty_millis);
+    println!("Sent packets.");
+    loop {
+        if let Ok(raw_packet) =
+            server_connection.read_raw_packet::<mcproto_rs::v1_16_3::RawPacket753>()
+        {
+            println!("Reading packet...");
+            match raw_packet {
+                mcproto_rs::v1_16_3::RawPacket753::StatusResponse(body) => {
+                    if let Ok(response) =
+                        mcproto_rs::v1_16_3::StatusResponseSpec::mc_deserialize(body.data)
+                    {
+                        println!(
+                            "Description: {}",
+                            response
+                                .value
+                                .response
+                                .description
+                                .to_traditional()
+                                .unwrap_or("none".to_owned())
+                        );
+                    };
+                }
+                _ => {}
+            };
+        };
+        std::thread::sleep(fifty_millis);
+    }
+}
+
+struct ServerConnection {
+    server_connection: mctokio::TcpConnection,
+}
+
+impl ServerConnection {
+    fn new(address: std::net::SocketAddr) -> Self {
+        let future = async move {
+            let connection = mctokio::TcpConnection::connect_to_server(address);
+            connection.await
+        };
+        let connection = tokio::runtime::Runtime::new().unwrap().block_on(future);
+        if let Ok(connected) = connection {
+            return Self {
+                server_connection: connected,
+            };
+        } else {
+            panic!("Failed to connect to server.");
+        };
+    }
+
+    fn read_raw_packet<'a, P>(&'a mut self) -> Result<P, String>
+    where
+        P: mcproto_rs::protocol::RawPacket<'a>,
+    {
+        let future = async move {
+            let read = self.server_connection.read_packet::<P>();
+            read.await
+        };
+        let thing = tokio::runtime::Runtime::new().unwrap().block_on(future);
+        return if let Ok(read) = thing {
+            match read {
+                Some(packet) => Ok(packet),
+                None => Err("Empty packet.".to_owned()),
+            }
+        } else {
+            println!("{:?}", thing.err());
+            Err("Packet read failed.".to_owned())
+        };
+    }
+
+    fn send_packet<P>(&mut self, packet: P) -> bool
+    where
+        P: mcproto_rs::protocol::Packet,
+    {
+        let future = async move {
+            let write = self.server_connection.write_packet(packet);
+            write.await
+        };
+        let thing = tokio::runtime::Runtime::new().unwrap().block_on(future);
+        if let Ok(_write) = &thing {
+            return true;
+        } else {
+            println!("{:?}", &thing.err());
+            return false;
+        };
+    }
 }
