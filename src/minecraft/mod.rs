3a681b9b66d0e56f3fdd3ce395928583f109401c
willemml <willem@leit.so>
2020-11-08 00:02:28 -0800

    documentation for minecraft::client

--- src/minecraft/mod.rs
+++ src/minecraft/mod.rs
@@ -2,5 +2,5 @@ pub mod client;
 pub mod status;
 pub mod net;
 
-use mcproto_rs::v1_16_3 as proto;
-use proto::{Packet753 as Packet, RawPacket753 as RawPacket};
+pub use mcproto_rs::v1_16_3 as proto;
+pub use proto::{Packet753 as Packet, RawPacket753 as RawPacket};
